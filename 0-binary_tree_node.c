dcc9a269108ef33bf2b9e0531571105f836f46d8
Binchat <binbin.he515@outlook.com>
2024-08-07 15:46:08 +0200

    modify the comments area with clear descrption

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - aa
- * @parent: aaa
- * @value: aaa
- * Return: aaa
+ * binary_tree_node - creates a binary tree node
+ * @parent: a pointer to the parent of the node to create
+ * @value: the value to put in the new node
+ * Return: a pointer to the newnode, otherwise NULL.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
