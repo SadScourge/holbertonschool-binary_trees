458a057dccb06c858e920b98bd8b30974d4d902e
Binchat <binbin.he515@outlook.com>
2024-08-08 23:39:07 +0200

    check betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -3,10 +3,10 @@
 /**
  * binary_tree_is_full - Checks if a binary tree is full.
  * @tree: A pointer to the root node of the tree to check.
- *
- * Return: If tree is NULL or is not full - 0.
- *         Otherwise - 1.
+ * Return: 0 if tree is NULL or is not full - 0.
+ * Otherwise - 1.
  */
+
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
