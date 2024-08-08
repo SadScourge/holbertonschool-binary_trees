41fc9a94f34657deb4dd154ac0f7f7cf93f0c9c3
Binchat <binbin.he515@outlook.com>
2024-08-08 02:02:01 +0200

    add files 3579-11-13

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0, else return height.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0;
+		size_t r = 0;
+
+		if (tree->left)
+		{
+			l = 1 + binary_tree_height(tree->left);
+		}
+		else
+		{
+			l = 0;
+		}
+
+		if (tree->right)
+		{
+			r = 1 + binary_tree_height(tree->right);
+		}
+		else
+		{
+			r = 0;
+		}
+		if (l > r)
+		{
+			return (l);
+		}
+		else
+		{
+			return (r);
+		}
+	}
+	return (0);
+}
